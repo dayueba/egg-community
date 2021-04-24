199a7c34cd8aaf4df7c75d2abb94eaf20ae27780
barbecue <1215007121@qq.com>
2021-04-25 00:21:41 +0800

    init project

---
+++ app/controller/home.js
@@ -0,0 +1,13 @@
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = {
+      desc: '社区项目-接口',
+      author: 'jiajun.pan',
+    };
+  }
+}
+
+module.exports = HomeController;
